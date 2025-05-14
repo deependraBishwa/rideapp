343a0a6643686129a8313797da5acced2fe1b29d
deependra bishwakarma <--replace-all>
2025-05-14 20:39:15 +0545

    webpage with

--- server.js
+++ server.js
@@ -1,82 +1,92 @@
 const express = require('express');
 const http = require('http');
-const { env } = require('process');
-const socketIo = require('socket.io');
+const bodyParser = require('body-parser');
+const {Server} = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-// const io = socketIo(server);
-const io = require('socket.io')(server, {
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+const io = new Server(server, {
     cors: {
-        origin: '*', // Allow from anywhere for testing
+        origin: '*',
         methods: ['GET', 'POST']
     }
 });
 
 const UserType = {
     USER: "USER",
-    RIDER: "RIDER",
-}
-let riders = {}
+    RIDER: "RIDER"
+};
+
+let connectRiders = {};
 let users = {};
-// Handle WebSocket connections
+
 io.on('connection', (socket) => {
-    console.log('connecteds:', socket.id);
+    const { userToken, latitude, longitude, name, userType } = socket.handshake.query;
 
-    const userId = socket.handshake.query.userId;
-    const latitude = socket.handshake.query.lat;
-    const longitude = socket.handshake.query.lng;
-    const name = socket.handshake.query.name;
-    const userType = socket.handshake.query.userType;
-
-    if (userType == UserType.USER) {
-
-        users[userId] = { userId: userId, socketId: socket.id, latitude, longitude, name, userType };
+    if (userType === UserType.USER) {
+        users[userToken] = {
+            token: userToken,
+            socketId: socket.id,
+            latitude,
+            longitude,
+            name,
+            userType
+        };
     } else {
-        riders[userId] = { userId: userId, socketId: socket.id, latitude, longitude, name, userType };
+        connectRiders[userToken] = {
+            token: userToken,
+            socketId: socket.id,
+            latitude,
+            longitude,
+            name,
+            userType
+        };
     }
 
-    console.log(socket.handshake.query);
-
-    socket.on('send_message', (data) => {
-        const recepientType = data.recepientType;
-        if (recepientType == UserType.USER) {
-            const user = users[data.recepient];
-            if (user && user.socketId) {
-                io.to(user.socketId).emit('receive_message', users);
-            } else {
-                console.log(`User ${data.recepient} not connected`);
-            }
-        } else {
-            const rider = riders[data.recepient];
-            if (rider && rider.socketId) {
-                io.to(rider.socketId).emit('receive_message', riders);
-            } else {
-                console.log(`User ${data.recepient} not connected`);
-            }
-        }
-    });
+    // Notify clients of the updated user list
+    io.emit('connected_users', Object.values(users));
 
     socket.on('disconnect', () => {
-
-        for (const id in riders) {
-            if (riders[id].socketId === socket.id) {
-                delete riders[id]
-                console.log(`${id} removed from riders`);
+        let removed = false;
+        for (const id in connectRiders) {
+            if (connectRiders[id].socketId === socket.id) {
+                delete connectRiders[id];
+                removed = true;
                 break;
             }
         }
+
         for (const id in users) {
             if (users[id].socketId === socket.id) {
                 delete users[id];
-                console.log(`${id} removed from users`);
+                removed = true;
                 break;
             }
+        }
+
+        if (removed) {
+            io.emit('disconnected', { socketId: socket.id });
         }
     });
 });
 
-const PORT = env.PORT || 3000
+app.post('/api/sendRequestToRider', (req, res) => {
+    const data = req.body;
+    console.log('Received data from Laravel:', data);
+
+    for (const riderId in connectRiders) {
+        const rider = connectRiders[riderId];
+        io.to(rider.socketId).emit('new_request', data);
+    }
+
+    res.json({ success: true, message: 'Request sent to all riders' });
+});
+
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, '0.0.0.0', () => {
-    console.log('Node.js WebSocket server running on port 3000');
+    console.log(`Server running on http://localhost:${PORT}`);
 });
